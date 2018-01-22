a1f91d2ba603ff94c0a5718968166089ae2aeb91
antoine.py@univ-fcomte.fr <guildantoine@hotmail.fr>
2018-01-22 20:53:50 +0100

    Ajout page commentaireJoueur.php

---
+++ ProjetV2/commentaireJoueur.php
@@ -0,0 +1,35 @@
+<?php
+include 'php/checkSession.php';
+?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+<head>
+    <title>Commentaires joueurs</title>
+    <link rel="icon" type="image/png" href="assets/images/fcsm.png" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/feuillematchstyle.css">
+    <link rel="stylesheet" type="text/css" href="css/navbarstyle.css">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <?php include 'navbar.php'; ?>
+        <div class="col-sm-8">
+            <div class="row">
+                <h1>
+                    Commentaires joueurs
+                </h1>
+            </div>
+            <div id="playersClub1"></div>
+            <div id="playersClub2"></div>
+
+            <datalist id="club1PlayersList"></datalist>
+            <datalist id="club2PlayersList"></datalist>
+
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="js/listesDeroulantes.js"></script>
+</body>
+</html>
--- ProjetV2/navbar.php
+++ ProjetV2/navbar.php
@@ -1,7 +1,7 @@
 <div class="col-sm-2" id="navbar">
 	<img src="assets/images/fcsm.png" class="img-fluid" width="50%" id="logonavbar">
 	<h1>Navigation</h1>
-	<a href="index.php">Acceuil</a>
+	<a href="index.php">Accueil</a>
 	<a href="feuilleMatch.php">Feuille de Match</a>
 	<a href="club.php">Clubs</a>
 	<a href="joueurs.php">Joueurs</a>
